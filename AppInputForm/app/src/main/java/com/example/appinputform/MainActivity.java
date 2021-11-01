c6f69822a4155d831d0a31f9e9a9a4cff8de8b4e
huongnvtv <huongitnv@gmail.com>
2021-11-01 15:34:08 +0700

    commit homeWork

---
+++ AppInputForm/app/src/main/java/com/example/appinputform/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.appinputform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText nameEditText = (EditText) findViewById(R.id.names);
+        String fullName = nameEditText.getText().toString();
+        RadioButton defaultGender = ( RadioButton ) findViewById ( R.id.male);
+        Boolean RadioButtonGender = defaultGender.isChecked ();
+        EditText mssvText = (EditText) findViewById(R.id.mssv);
+        String mssv = mssvText.getText().toString();
+        EditText birthday = (EditText) findViewById(R.id.birthday) ;
+        birthday.addTextChangedListener(new TextWatcher() {
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]", "");
+                    String cleanC = current.replaceAll("[^\\d.]", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        int day  = Integer.parseInt(clean.substring(0,2));
+                        int mon  = Integer.parseInt(clean.substring(2,4));
+                        int year = Integer.parseInt(clean.substring(4,8));
+
+                        if(mon > 12) mon = 12;
+                        cal.set(Calendar.MONTH, mon-1);
+
+                        year = (year<1900)?1900:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d",day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    birthday.setText(current);
+                    birthday.setSelection(sel < current.length() ? sel : current.length());
+
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        EditText phoneNumberText = (EditText) findViewById(R.id.phoneNumber);
+        String phoneNumber = phoneNumberText.getText().toString();
+        EditText emailText = (EditText) findViewById(R.id.email);
+        String email = emailText.getText().toString();
+        EditText addressText = (EditText) findViewById(R.id.address);
+        String address = addressText.getText().toString();
+    }
+    public void submitbuttonHandler (View view) {
+
+    }
+
+}
